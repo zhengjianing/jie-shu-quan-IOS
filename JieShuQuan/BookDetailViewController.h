c20fdae25dffae770521defd1e1555150d24deaf
Zheng Jia Ning <jianingzheng@gmail.com>
2014-08-24 21:09:46 +0800

    create BookDetailViewController and its view.

---
+++ JieShuQuan/BookDetailViewController.h
@@ -0,0 +1,22 @@
+//
+//  BookDetailViewController.h
+//  JieShuQuan
+//
+//  Created by Jianning Zheng on 8/24/14.
+//  Copyright (c) 2014 JNXZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BookDetailViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *bookImageView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *authorsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *publisherLabel;
+@property (weak, nonatomic) IBOutlet UILabel *publishDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *priceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *discriptionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *authorInfoLabel;
+
+@end
