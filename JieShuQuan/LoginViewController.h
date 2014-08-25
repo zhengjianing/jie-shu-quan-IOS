cdbe69d56aa1896e0dac8a8ee5b92a881be36736
Yang Xizozhu <yangzhuxiao@126.com>
2014-08-25 19:37:44 +0800

    added LoginViewController and updated stroyboard

---
+++ JieShuQuan/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  JieShuQuan
+//
+//  Created by Yang Xiaozhu on 14-8-25.
+//  Copyright (c) 2014年 JNXZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
