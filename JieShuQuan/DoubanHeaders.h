573b570d94ab5afeda48f2bdde3fa7cf22706031
Yang Xizozhu <yangzhuxiao@126.com>
2014-08-25 20:22:31 +0800

    changed 'DoubanHeaders.h', now the Douban Login view can display properly

--- JieShuQuan/DoubanHeaders.h
+++ JieShuQuan/DoubanHeaders.h
@@ -6,8 +6,10 @@
 //  Copyright (c) 2014年 Xiaozhu. All rights reserved.
 //
 
-#define kAPIKey @"replace with your own API Key"
-#define kSecret @"replace with your own secret"
+#define kAPIKey @"0071e999e5b2c9100155bccdb1185b30"
+#define kSecret @"4b9b46bad25096f4"
 #define kAuthorizeURL @"https://www.douban.com/service/auth2/auth"
 #define kTokenURL @"https://www.douban.com/service/auth2/token"
-#define kRedirect_URL @"replace with your own redirect_URL"
+#define kRedirect_URL @"http://www.douban.com"
+
+#define kSearchURL @"https://api.douban.com/v2/book/search"
