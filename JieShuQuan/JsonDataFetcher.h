145676cc9779b7e24a0079ba9e9b40f72e8128de
Yang Xizozhu <yangzhuxiao@126.com>
2014-08-27 20:52:01 +0800

    decided not to use framework now, created JsonDataFetcher to fetch searched json results

---
+++ JieShuQuan/JsonDataFetcher.h
@@ -0,0 +1,15 @@
+//
+//  JsonDataFetcher.h
+//  JieShuQuan
+//
+//  Created by Yang Xiaozhu on 14-8-27.
+//  Copyright (c) 2014年 JNXZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JsonDataFetcher : NSObject
+
++ (void)dataFromURL:(NSString *)url withCompletion:(void (^)(NSData *jsonData))completionBlock;
+
+@end
---
+++ JieShuQuan/JsonDataFetcher.m
@@ -0,0 +1,28 @@
+//
+//  JsonDataFetcher.m
+//  JieShuQuan
+//
+//  Created by Yang Xiaozhu on 14-8-27.
+//  Copyright (c) 2014年 JNXZ. All rights reserved.
+//
+
+#import "JsonDataFetcher.h"
+
+@implementation JsonDataFetcher
+
++ (void)dataFromURL:(NSURL *)url withCompletion:(void (^)(NSData *jsonData))completionBlock
+{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (data) {
+                completionBlock(data);
+            } else {
+                NSLog(@"Data Fetching failed...");
+                abort();
+            }
+        });
+    });
+}
+
+@end
