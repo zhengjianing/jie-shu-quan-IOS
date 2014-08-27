86e4c054e4a68f6e6c8a1d402858e6fdce694f3a
Yang Xizozhu <yangzhuxiao@126.com>
2014-08-27 20:20:05 +0800

    added SearchTableViewController in storyboard and created SearchTableViewController.h/m, next is to implement .m file

---
+++ JieShuQuan/SearchTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchTableViewController.h
+//  JieShuQuan
+//
+//  Created by Yang Xiaozhu on 14-8-27.
+//  Copyright (c) 2014年 JNXZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchTableViewController : UITableViewController
+
+@end
