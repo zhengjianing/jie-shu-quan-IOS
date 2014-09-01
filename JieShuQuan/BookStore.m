c971acf233ca17965ae06ced47cefbd6b20bfe0d
Yang Xizozhu <yangzhuxiao@126.com>
2014-09-01 23:43:57 +0800

    in order for app to run correctly, comment out the code in - (void)createBooks of BookStore

--- JieShuQuan/BookStore.m
+++ JieShuQuan/BookStore.m
@@ -27,11 +27,11 @@
     _allBooks = [[NSMutableArray alloc] init];
     
     NSArray *authors = [[NSArray alloc] initWithObjects:@"author-1", @"author-2", nil];
-    Book *book = [[Book alloc] initWithName:@"IOS编程" authors:authors imageHref:@"http://img5.douban.com/mpic/s9042517.jpg" discription:@"book discription" authorInfo:@"author info" price:@"29.0" publisher:@"publisher" publishDate:@"2013-03-02"];
-    [_allBooks addObject:book];
-    
-    book = [[Book alloc] initWithName:@"一个陌生女子的来信" authors:authors imageHref:@"http://img3.douban.com/mpic/s1119643.jpg" discription:@"book discription" authorInfo:@"author info" price:@"29.0" publisher:@"publisher" publishDate:@"2013-03-02"];
-    [_allBooks addObject:book];
+//    Book *book = [[Book alloc] initWithName:@"IOS编程" authors:authors imageHref:@"http://img5.douban.com/mpic/s9042517.jpg" description:@"book discription" authorInfo:@"author info" price:@"29.0" publisher:@"publisher" publishDate:@"2013-03-02"];
+//    [_allBooks addObject:book];
+//    
+//    book = [[Book alloc] initWithName:@"一个陌生女子的来信" authors:authors imageHref:@"http://img3.douban.com/mpic/s1119643.jpg" discription:@"book discription" authorInfo:@"author info" price:@"29.0" publisher:@"publisher" publishDate:@"2013-03-02"];
+//    [_allBooks addObject:book];
     
 }
 
