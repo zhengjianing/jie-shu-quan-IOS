3762b0e2c4001b6f29d60de8400eec1b9e1cd140
Zheng Jia Ning <jianingzheng@gmail.com>
2014-09-27 19:44:35 +0800

    create feedbackViewController

---
+++ JieShuQuan/FeedbackViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedbackViewController.h
+//  JieShuQuan
+//
+//  Created by Jianning Zheng on 9/27/14.
+//  Copyright (c) 2014 JNXZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedbackViewController : UIViewController
+
+@end
---
+++ JieShuQuan/FeedbackViewController.m
@@ -0,0 +1,23 @@
+//
+//  FeedbackViewController.m
+//  JieShuQuan
+//
+//  Created by Jianning Zheng on 9/27/14.
+//  Copyright (c) 2014 JNXZ. All rights reserved.
+//
+
+#import "FeedbackViewController.h"
+
+@interface FeedbackViewController ()
+
+@end
+
+@implementation FeedbackViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+}
+
+@end
