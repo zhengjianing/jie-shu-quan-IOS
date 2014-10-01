f95e4e1569f4b24baff1453506484e046fd96edf
Zheng Jia Ning <jianingzheng@gmail.com>
2014-10-01 21:30:54 +0800

    point to the new api server

--- JieShuQuan/ServerHeaders.h
+++ JieShuQuan/ServerHeaders.h
@@ -6,23 +6,23 @@
 //  Copyright (c) 2014 JNXZ. All rights reserved.
 //
 
-#define kRegisterURL @"http://jie-shu-quan.herokuapp.com/register"
-#define kLoginURL @"http://jie-shu-quan.herokuapp.com/login"
-#define kUploadAvatarURL @"http://jie-shu-quan.herokuapp.com/upload_avatar"
-#define kAvatarURLPrefix @"http://jie-shu-quan.herokuapp.com/assets/userAvatar"
+#define kRegisterURL @"http://jieshuquan.herokuapp.com/register"
+#define kLoginURL @"http://jieshuquan.herokuapp.com/login"
+#define kUploadAvatarURL @"http://jieshuquan.herokuapp.com/upload_avatar"
+#define kAvatarURLPrefix @"http://jieshuquan.herokuapp.com/assets/userAvatar"
 
-#define kAddBookURL @"http://jie-shu-quan.herokuapp.com/add_book"
-#define kDeleteBookURL @"http://jie-shu-quan.herokuapp.com/remove_book"
-#define kGetBookCommentsURL @"http://jie-shu-quan.herokuapp.com/comments_for_book/"
-#define kPostBookCommentURL @"http://jie-shu-quan.herokuapp.com/comments/create"
+#define kAddBookURL @"http://jieshuquan.herokuapp.com/add_book"
+#define kDeleteBookURL @"http://jieshuquan.herokuapp.com/remove_book"
+#define kGetBookCommentsURL @"http://jieshuquan.herokuapp.com/comments_for_book/"
+#define kPostBookCommentURL @"http://jieshuquan.herokuapp.com/comments/create"
 
-#define kChangeBookStatusURL @"http://jie-shu-quan.herokuapp.com/change_status"
-#define kChangeUserNameURL @"http://jie-shu-quan.herokuapp.com/change_username"
-#define kChangeUserPhoneNumberURL @"http://jie-shu-quan.herokuapp.com/change_phone_number"
-#define kChangeUserLocationURL @"http://jie-shu-quan.herokuapp.com/change_location"
+#define kChangeBookStatusURL @"http://jieshuquan.herokuapp.com/change_status"
+#define kChangeUserNameURL @"http://jieshuquan.herokuapp.com/change_username"
+#define kChangeUserPhoneNumberURL @"http://jieshuquan.herokuapp.com/change_phone_number"
+#define kChangeUserLocationURL @"http://jieshuquan.herokuapp.com/change_location"
 
-#define kMyFriendsURL @"http://jie-shu-quan.herokuapp.com/friends_by_user/"
-#define kMyBooksURL @"http://jie-shu-quan.herokuapp.com/books_by_user/"
-#define kMyFriendsWithBookURL @"http://jie-shu-quan.herokuapp.com/friendsWithBook/"
+#define kMyFriendsURL @"http://jieshuquan.herokuapp.com/friends_by_user/"
+#define kMyBooksURL @"http://jieshuquan.herokuapp.com/books_by_user/"
+#define kMyFriendsWithBookURL @"http://jieshuquan.herokuapp.com/friendsWithBook/"
 
 
